1d451ed362e947b34117035f948a6f6bf63af1eb
Frank Bold Society <servis@frankbold.org>
2024-06-05 11:43:31 +0200

    fix

--- docassemble/DAZakladacSpolku/interviewTools.py
+++ docassemble/DAZakladacSpolku/interviewTools.py
@@ -1,4 +1,4 @@
-from nocodb import list_nocodb_record
+from DAZakladacSpolku.nocodb import list_nocodb_record
 
 def get_questions_from_nocodb(table_id: str, filter: str= ""):
     data = list_nocodb_record(table_id=table_id, fields="label,field,datatype,input type,help", filter=filter)
