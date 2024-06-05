a7e20f632d02dcd9e009617ea00914e00b1406a6
Frank Bold Society <servis@frankbold.org>
2024-06-05 11:41:49 +0200

    update to question gen

---
+++ docassemble/DAZakladacSpolku/interviewTools.py
@@ -0,0 +1,11 @@
+from nocodb import list_nocodb_record
+
+def get_questions_from_nocodb(table_id: str, filter: str= ""):
+    data = list_nocodb_record(table_id=table_id, fields="label,field,datatype,input type,help", filter=filter)
+
+    updated_data = []
+    for entry in data:
+        updated_entry = {k: v for k, v in entry.items() if v is not None}
+        updated_data.append(updated_entry)
+
+    return updated_data
