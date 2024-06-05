a700f5994d605284662c076a32f6cef3029a3050
Frank Bold Society <servis@frankbold.org>
2024-06-05 11:47:48 +0200

    text_fix

--- docassemble/DAZakladacSpolku/interviewTools.py
+++ docassemble/DAZakladacSpolku/interviewTools.py
@@ -1,5 +1,4 @@
-import requests
-from docassemble.base.util import get_config
+from docassemble.DAZakladacSpolku.nocodb import list_nocodb_record
 
 def get_questions_from_nocodb(table_id: str, filter: str= ""):
     data = list_nocodb_record(table_id=table_id, fields="label,field,datatype,input type,help", filter=filter)
@@ -10,39 +9,3 @@ def get_questions_from_nocodb(table_id: str, filter: str= ""):
         updated_data.append(updated_entry)
 
     return updated_data
-
-def list_nocodb_record(table_id: str, fields: str = "", viewId: str = "", filter: str= ""):
-
-    url = f"{get_config('nocodbUrl')}/api/v2/tables/{table_id}/records"
-    
-    headers = {
-        "xc-token": get_config('nocodbToken'),
-    }
-
-    params = {
-        "offset": 0,
-        "fields": fields,
-        "viewId": viewId,
-        "where": filter
-    }
-
-    all_records = []
-
-    while True:
-        response = requests.get(f"{get_config('nocodbUrl')}/api/v2/tables/{table_id}/records", headers=headers, params=params)
-        if response.status_code != 200:
-            print(f"Failed to fetch records: {response.text}")
-            break
-
-        data = response.json()
-        all_records.extend(data.get('list', []))
-        
-        # Pagination handling
-        page_info = data.get('pageInfo', {})
-        print(page_info)
-        if page_info.get('isLastPage'):
-            break
-        else:
-            params['offset'] += 25
-    
-    return all_records
