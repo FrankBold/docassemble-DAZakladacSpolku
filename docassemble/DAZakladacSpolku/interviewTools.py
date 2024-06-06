a73e2139105ce041d01d6472b24110003c168212
Frank Bold Society <servis@frankbold.org>
2024-06-06 16:38:56 +0200

    fix

--- docassemble/DAZakladacSpolku/interviewTools.py
+++ docassemble/DAZakladacSpolku/interviewTools.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+from docassemble.base.util import validation_error
+
 from docassemble.DATools.nocodb import list_nocodb_record, update_record
 
 def get_questions_from_nocodb(table_id: str, filter: str= ""):
@@ -63,3 +65,15 @@ def flatten_json(data, prefix=''):
             result[new_key] = value
 
     return result
+
+def contains_spolek(x):
+  x = x.lower()
+  if "spolek" in x:
+    return True
+  elif "z. s." in x:
+    return True
+  elif "zapsaný spolek" in x:
+    return True
+  else:
+    validation_error('Název spolku <strong>musí</strong> obsahovat "z. s.", "spolek", nebo "zapsaný spolek"')
+  return
