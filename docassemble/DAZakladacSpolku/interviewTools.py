5ecd3d234a1f25e51ae3536550e9f8b786ee7be1
Frank Bold Society <servis@frankbold.org>
2024-06-11 17:23:34 +0200

    fix

--- docassemble/DAZakladacSpolku/interviewTools.py
+++ docassemble/DAZakladacSpolku/interviewTools.py
@@ -21,7 +21,6 @@ def save_spolek_data(data: dict):
 
     data = {
         "dataSpolek": json.dumps(data["Spolek"])
-        #"dataSpolek": json.dumps(flatten_json(data["Spolek"]))
     }
     #temp
     results = update_record(table_id="mkejxthrd05vdcc", content=data, row_id=row_id)
